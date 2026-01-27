6a39a8a0e7d72c9d4cc7eb0051f80d2c3e2f93d3
codercoder61 <mohamedknoud@gmail.com>
2026-01-27 18:14:20 +0100

    Update skills.tsx

--- components/skills.tsx
+++ components/skills.tsx
@@ -9,7 +9,7 @@ const skillCategories = [
   },
   {
     category: 'Backend Development',
-    skills: ['Laravel','Express.js','Flask','MySQL', 'Node.js', 'Python', 'PostgreSQL', 'MongoDB', 'Firebase'],
+    skills: ['Laravel','Express.js','Flask','MySQL', 'Node.js', 'Python', 'MongoDB', 'Firebase'],
   },
   {
     category: 'Tools & Platforms',
